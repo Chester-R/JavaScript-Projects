f229a958da23a62950af875050a72c76439de06e
Chester-R <chesterrimando96@gmail.com>
2020-09-19 10:21:50 -0400

    Assignment 7
    Submitting assignment 7 html and java script

--- Basic Javascript Project/JS/Basic_JavaScript_7.js
+++ Basic Javascript Project/JS/Basic_JavaScript_7.js
@@ -1,16 +1,39 @@
-function Vehicle(Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + x + "<br>");
+}
+function Add_numbers_2() {
+    document.write(x + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+if (4<20){
+    document.write("<br>Left is less than right.")
 }
 
-var Jack = new Vehicle("Dodge","Viper",2020,"Red")
-var Emily = new Vehicle("Jeep","Trail Hawk",2019,"White and Black")
-var Erik = new Vehicle("Ford","Pinto",1971,"Mustard")
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote="You are old enough to vote.";
+    }
+    else {
+        Vote = "You are not old enough to vote.";
+    }
+    document.getElementById("How_old_are_you?").innerHTML=Vote;
+}
 
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML=
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " +Erik.Vehicle_Model+
-    " manufactured in " + Erik.Vehicle_Year;
+function Time_function() {
+    var Time= new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "Morning Time!";
+    }
+    else if (Time >12 == Time < 18) {
+        Reply = "Afternoon Time!";
+    }
+    else {
+        Reply = "Evening Time!";
+    }
+    document.getElementById("Time_of_day").innerHTML=Reply;
 }
--- Basic Javascript Project/JS/Example.js
+++ Basic Javascript Project/JS/Example.js
@@ -1,3 +1,9 @@
 function my_Function() {
     document.getElementById("Test").innerHTML= isNaN('0087')
 }
+
+function get_Date() {
+    if (new Date().getHours() <18) {
+        document.getElementById("Greeting").innerHTML="How are you today?";
+    }
+}
